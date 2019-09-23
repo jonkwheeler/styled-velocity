8fd60aefb2825069b8c5138294e4380a5421a1f8
Jon Wheeler <jon.k.wheeler@gmail.com>
2019-09-23 09:11:50 +0100

    filter out the baddies

--- src/shared-props/gridProps.ts
+++ src/shared-props/gridProps.ts
@@ -5,12 +5,14 @@ import { css } from 'styled-components'
 
 export const gridProps = css`
   ${props =>
-    gridPropsAvailable.map(
-      ({ prop, property, conversionType }: PropsAvailablePropTypes) =>
-        props[prop] &&
-        createStyle({
-          property,
-          value: conversionType ? convertValue(props[prop], conversionType) : props[prop],
-        }),
-    )};
+    gridPropsAvailable
+      .map(({ prop, property, conversionType }: PropsAvailablePropTypes) =>
+        props[prop]
+          ? createStyle({
+              property,
+              value: conversionType ? convertValue(props[prop], conversionType) : props[prop],
+            })
+          : null,
+      )
+      .filter(item => item !== null)};
 `
