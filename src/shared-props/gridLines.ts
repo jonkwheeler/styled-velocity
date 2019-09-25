a659e471f4d0ba81876a4c1e4361cd22d14d50d4
Jon Wheeler <jon.k.wheeler@gmail.com>
2019-09-25 18:03:59 +0100

    fix gridLines

--- src/shared-props/gridLines.ts
+++ src/shared-props/gridLines.ts
@@ -16,11 +16,11 @@ export const gridLines = css`
     }
     /* base this on cells prop */
     &:before { 
-      background: repeating-linear-gradient(to right, green, white 2.5vw);
+      background: repeating-linear-gradient(to right, green, white var(--cel, 2.5vw));
       @media(min-width: 2000px){ background: repeating-linear-gradient(to bottom, green, white 50px); }
     }
     &:after { 
-      background: repeating-linear-gradient(to bottom, red, white 2.5vw);
+      background: repeating-linear-gradient(to bottom, red, white var(--cel, 2.5vw));
       @media(min-width: 2000px){ background: repeating-linear-gradient(to bottom, red, white 50px); }
     }
 
