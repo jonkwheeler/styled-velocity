32fa90b76234d86e343a76656d8dd5e6310e1e29
Jon Wheeler <jon.k.wheeler@gmail.com>
2020-01-21 14:12:28 +0000

    add mq to calcCells

--- src/utilities/style/createStyle/calcCells.ts
+++ src/utilities/style/createStyle/calcCells.ts
@@ -1,2 +1,4 @@
 export const calcCells = (property: string, cells: number) =>
-  `${property}: ${2.5 * cells}vw;${property}: calc(var(--cel,2.5vw) * ${cells});`
+  `${property}: ${2.5 *
+    cells}vw; ${property}: calc(var(--cel,2.5vw) * ${cells}); @media (min-width: 2000px){ ${property}: ${50 *
+    cells}px;}`
